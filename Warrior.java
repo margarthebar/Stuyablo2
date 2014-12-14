a8ca97c0c20cf5a906bef85c8114b6c223a322a8
margarthebar <margarburkart@gmail.com>
2014-12-14 10:46:44 -0500

    replaced wrong file

--- Warrior.java
+++ Warrior.java
@@ -1,36 +1,36 @@
-public class Wizard extends Adventurer{
-    private int mana;
+public class Warrior extends Adventurer implements Comparable<Warrior>{
+    private int rage;
 
-    public Wizard(){
-	this("Tim",20);
+    public Warrior(){
+        this("Ted",30);
     }
-    public Wizard(String s){
-        this(s,20);
+    public Warrior(String s){
+	this(s,30);
     }
-    public Wizard(String s, int startHP){
+    public Warrior(String s, int startHP){
 	super(s,startHP);
-	setMana(25);
-	Str = 6;
-	Dex = 8;
-	Int = 18;
+	setRage(20);
+	Str = 14;
+	Dex = 6;
+	Int = 10;
+    }
+    
+    public int getRage(){
+	return rage;
     }
 
-    public int getMana(){
-	return mana;
+    public void setRage(int n){
+	rage = n;
     }
-    public void setMana(int n){
-	mana = n;
-    }
+
     public void setSpecial(int n){
-	setMana(n);
+	setRage(n);
     }
-    public boolean hitMagic(Adventurer other){
-	return rand.nextDouble() < ((double)(Int - other.Int + 6)/20);
-    }
+
     public void attack(Adventurer other){
 	System.out.println(this.getName() + " attacks " + other.getName());
 	if(hit(other)){
-	    int damage = (rand.nextInt(Str) + 1);
+	    int damage = (rand.nextInt(Str) + 4);
 	    other.setHP(other.getHP() - damage);
 	    System.out.println("and hits for " + damage + " points of damage");
 	}else{
@@ -38,25 +38,20 @@ public class Wizard extends Adventurer{
 	}
     }
     public void specialAttack(Adventurer other){
-	if(mana >= 5){
-	    System.out.println(this.getName() + " hurls fire at " + other.getName());
-	    if(hitMagic(other)){
-		int damage = (rand.nextInt(Int) + 1 + rand.nextInt(10) + 1);
-		other.setHP(other.getHP() - damage);
-		System.out.println("and burns for " + damage + " points of damage");
-	    }else{
-		System.out.println("and misses");
-	    }
-	    mana -= 5;
+	System.out.println(this.getName() + " hacks madly at " + other.getName());
+	if(hit(other)){
+	    int damage = (rand.nextInt(Str) + 1 + rand.nextInt(rage/2) + 1);
+	    other.setHP(other.getHP() - damage);
+	    System.out.println("and hits for " + damage + " points of damage");
 	}else{
-	    System.out.println("You do not have enough mana to perform this action.");
+	    System.out.println("and misses");
 	}
     }
     public String getStats(){
-	return super.getStats() +" "+ mana+"MANA";
+	return super.getStats() +" "+ rage+"RAGE";
     }
 
-    public boolean canSpecialAttack(){
-	return mana >= 5;
+    public int compareTo(Warrior other){
+	return other.getHP() - this.getHP();
     }
 }
